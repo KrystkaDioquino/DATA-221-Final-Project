2c95a1376d74a23f42b55b3d357e3180d7ffe9a1
aleenairfan <aleenairfann@gmail.com>
2026-03-22 20:59:41 -0600

    Data cleaning and preprocessing

--- DecisionTreeModel.py
+++ DecisionTreeModel.py
@@ -1,1 +1,22 @@
-#initializa
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+
+# DATASET PREPARATION
+
+# Load the dataset into pandas DataFrame
+df = pd.read_csv("StudentsPerformance.csv")
+
+
+# DATA CLEANING AND PROCESSING
+
+# Check for missing values
+print("Missing values:\n", df.isna().sum())
+
+# Check for duplicate rows
+print("Duplicates:", df.duplicated().any())
+
+# Remove reading and writing scores to prevent data leakage
+df = df.drop(columns=["reading score", "writing score"])
